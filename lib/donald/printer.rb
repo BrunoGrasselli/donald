1ebacce067ae8c60cb4d3a53ee333f74f40a8fdc
BrunoGrasselli <bruno.grasselli@gmail.com>
2010-08-21 15:02:49 -0300

    Printer refactor

--- lib/donald/printer.rb
+++ lib/donald/printer.rb
@@ -5,18 +5,28 @@ module Donald
     end
 
     def print_files(files)
-      print_delimiter
-      files.each {|f| @output.puts f}
-      print_delimiter
+      print_delimiter do
+        files.each {|f| @output.puts f}
+      end
     end
     
     def print_no_files_message
-      print_delimiter
-      @output.puts 'No unmerged files found'
-      print_delimiter
+      print_delimiter do
+        @output.puts 'No unmerged files found'
+      end
     end
     
+    private
+
     def print_delimiter
+      print_single_delimiter
+      if block_given?
+        yield
+        print_single_delimiter
+      end
+    end
+
+    def print_single_delimiter
       @output.puts '*' * 30
     end
   end
