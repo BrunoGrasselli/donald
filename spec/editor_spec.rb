d962a1af7a074c0f1f4e4dcd587b304753d6bbd6
BrunoGrasselli <bruno.grasselli@gmail.com>
2010-08-22 15:42:52 -0300

    Changing tests

--- spec/editor_spec.rb
+++ spec/editor_spec.rb
@@ -1,38 +1,49 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Donald::Editor do
-  context 'with $EDITOR variable not seted up' do
-    before :each do
-      Donald::Environment.stub!(:editor_variable).and_return(nil)
-    end
-
-    context 'with no argument' do
-      it 'should return the default editor' do
-        Donald::Editor.new.instance_variable_get('@name').should eql(Donald::Editor::DEFAULT_EDITOR)
-      end
-    end
-
-    context 'with mate as editor argument' do
-      it 'should return mate' do
-        Donald::Editor.new('mate').instance_variable_get('@name').should eql('mate')
-      end
-    end
+  before :each do
+    Kernel.stub!(:system)
+    @files = ["app/models/author.rb"]
   end
 
-  context 'with $EDITOR variable as gedit' do
-    before :each do
-      Donald::Environment.stub!(:editor_variable).and_return('gedit')
-    end
+  describe '#run' do
+    context 'with $EDITOR variable not seted up' do
+      before :each do
+        Donald::Environment.stub!(:editor_variable).and_return(nil)
+      end
 
-    context 'with no argument' do
-      it 'should return gedit' do
-        Donald::Editor.new.instance_variable_get('@name').should eql('gedit')
+      context 'with no argument' do
+        it 'should run the default editor' do
+          Kernel.should_receive(:system).with("#{Donald::Editor::DEFAULT_EDITOR} -p +/HEAD app/models/author.rb")
+          Donald::Editor.new.run(@files)
+        end
+      end
+
+      context 'with mate as editor argument' do
+        it 'should run mate' do
+          Kernel.should_receive(:system).with('mate app/models/author.rb')
+          Donald::Editor.new('mate').run(@files)
+        end
       end
     end
 
-    context 'with vim as argument' do
-      it 'should return vim' do
-        Donald::Editor.new('vim').instance_variable_get('@name').should eql('vim')
+    context 'with $EDITOR variable as gedit' do
+      before :each do
+        Donald::Environment.stub!(:editor_variable).and_return('gedit')
+      end
+
+      context 'with no argument' do
+        it 'should run gedit' do
+          Kernel.should_receive(:system).with('gedit app/models/author.rb')
+          Donald::Editor.new.run(@files)
+        end
+      end
+
+      context 'with vim as argument' do
+        it 'should run vim' do
+          Kernel.should_receive(:system).with('vim -p +/HEAD app/models/author.rb')
+          Donald::Editor.new('vim').run(@files)
+        end
       end
     end
   end
